a622d75f2c1565bf8f657cdf0c1036a6298e55ab
MagusConjurer <cameronisadavis@gmail.com>
2024-04-15 09:03:07 -0600

    Scaffold the database

---
+++ Phase3/LMSHandout/LMS/Models/LMSModels/AssignmentCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LMS.Models.LMSModels
+{
+    public partial class AssignmentCategory
+    {
+        public AssignmentCategory()
+        {
+            Assignments = new HashSet<Assignment>();
+        }
+
+        public uint CategoryId { get; set; }
+        public uint Weight { get; set; }
+        public string Name { get; set; } = null!;
+        public uint? ClassId { get; set; }
+
+        public virtual Class? Class { get; set; }
+        public virtual ICollection<Assignment> Assignments { get; set; }
+    }
+}
---
+++ Phase3/LMSHandout/LMS/Models/LMSModels/Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LMS.Models.LMSModels
+{
+    public partial class Class
+    {
+        public Class()
+        {
+            AssignmentCategories = new HashSet<AssignmentCategory>();
+            EnrollmentGrades = new HashSet<EnrollmentGrade>();
+        }
+
+        public uint ClassId { get; set; }
+        public uint SemesterYear { get; set; }
+        public string Season { get; set; } = null!;
+        public string Location { get; set; } = null!;
+        public TimeOnly Start { get; set; }
+        public TimeOnly End { get; set; }
+        public string TaughtBy { get; set; } = null!;
+        public uint CourseId { get; set; }
+
+        public virtual Course Course { get; set; } = null!;
+        public virtual Professor TaughtByNavigation { get; set; } = null!;
+        public virtual ICollection<AssignmentCategory> AssignmentCategories { get; set; }
+        public virtual ICollection<EnrollmentGrade> EnrollmentGrades { get; set; }
+    }
+}
